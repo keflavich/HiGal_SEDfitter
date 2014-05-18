666d36d241ff3daba26d830e958115518009d47e
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-05-18 15:21:47 +0200

    added higal_beams (required)

---
+++ higal_sedfitter/higal_beams.py
@@ -0,0 +1,15 @@
+from astropy import units as u
+
+# Values extracted from Traficante 2011 Table 2
+# http://adsabs.harvard.edu/abs/2011MNRAS.416.2932T
+beams = {70: 10.7*u.arcsec,
+         160: 13.9*u.arcsec,
+         250: 23.9*u.arcsec,
+         350: 31.3*u.arcsec,
+         500: 43.8*u.arcsec}
+
+name_to_um = {'blue':70,
+              'red':160,
+              'PSW':250,
+              'PMW':350,
+              'PLW':500}
